ab5e667fbe3eaaa6264fb3ee83348f482cac5374
ssdorwat <ssdorwat@ncsu.edu>
2018-07-15 22:56:09 -0400

    done

--- currency-converter.js
+++ currency-converter.js
@@ -3,15 +3,25 @@ const axios = require('axios')
 const getExchangeRate = async (from, to) => {
     const response = await  axios.get('http://data.fixer.io/api/latest?access_key=7793ad77f7adabffbda839e8575a3a10&format=1')
     const euroValue =  1 / response.data.rates[from];
-    const to_value = euroValue * response.data.rates[to]
-    return to_value
+    const rate = euroValue * response.data.rates[to]
+    return rate
 }
 
-const  getCurrencies = async (currencyCode) => {
+const  getCountries = async (currencyCode) => {
      const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
      return response.data.map( country => country.name)
 }
 
-// getExchangeRate('USD', 'CAD').then(val => console.log(val)).catch(e => console.log(e))
 
-getCurrencies('EUR').then( res => console.log(res)).catch( e => console.log(e) )
+const convertCurrency = async (from, to, amount) => {
+    const rate = await getExchangeRate(from, to);
+    const convertedAmount = (rate * amount).toFixed(2);
+    console.log(convertedAmount);
+    const countries = await getCountries(to)
+    return `${amount} ${from} is equivalent to ${convertedAmount} ${to}. You can spend these in following countries: ${countries.join(', ')}`
+}
+
+convertCurrency('USD', 'USD',20).then(res => console.log(res))
+
+// getExchangeRate('USD', 'CAD').then(val => console.log(val)).catch(e => console.log(e))
+// getCurrencies('EUR').then( res => console.log(res)).catch( e => console.log(e) )
